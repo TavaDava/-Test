6899f29aa0c3276c71e7a81b275f5f5a283f94c0
DavidTavakalov <tavakalovdavid73@gmail.com>
2024-03-22 19:38:49 +0300

    Все задания

---
+++ Task02_Sem7/Program.cs
@@ -0,0 +1,31 @@
+﻿/// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+/// Даны два неотрицательных числа m и n.
+
+using System;
+
+class Program
+{
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0 && m > 0)
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+
+    static void Main()
+    {
+        int m = 3; // Замените 3 на желаемое значение m
+        int n = 2; // Замените 2 на желаемое значение n
+
+        Console.WriteLine($"Ackermann({m}, {n}) = {Ackermann(m, n)}");
+    }
+}
---
+++ Task03_Sem7/Program.cs
@@ -0,0 +1,26 @@
+﻿/// Задайте произвольный массив. 
+/// Выведете его элементы, начиная с конца. 
+/// Использовать рекурсию, не использовать циклы.
+
+
+using System;
+
+class Program
+{
+    static void PrintArrayReverse(int[] array, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write(array[index] + " ");
+            PrintArrayReverse(array, index - 1);
+        }
+    }
+
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 }; // Замените этот массив на произвольный массив
+
+        Console.Write("Элементы массива в обратном порядке: ");
+        PrintArrayReverse(array, array.Length - 1);
+    }
+}
